0dd8a2cee7e6c65f8263d35c42d9cfab12d63834
murgascarrillo <conclave@tutanota.com>
2025-11-03 20:34:52 -0500

    mailing

--- scripts.js
+++
@@ -1,4 +0,0 @@
-function escapeHtml(s){ return s
-  .replaceAll('&','&amp;').replaceAll('<','&lt;')
-  .replaceAll('>','&gt;').replaceAll('"','&quot;').replaceAll(/'/g, '&#39;'); ; }
-document.body.innerText = escapeHtml(userProvided);
